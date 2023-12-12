9fd64f04a43614f36c9a4dab38cd7f04ec4c4bb7
Aman Pandey <aman2001mi@gmail.com>
2023-12-12 22:42:36 +0530

    updated settings env files

--- src/vendor_management_system/settings.py
+++ src/vendor_management_system/settings.py
@@ -14,6 +14,11 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# load environment variables from env file at the root
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9o-%qprfet^wnt@(3*1d2f)&nm@6g)!m&b^h&jmmkzh@4)%#kp"
+SECRET_KEY = (
+    os.environ.get("SECRET_KEY")
+    or "django-insecure-9o-%qprfet^wnt@(3*1d2f)&nm@6g)!m&b^h&jmmkzh@4)%#kp"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
